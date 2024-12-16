c78fd81ee72326cdb425a4490ef38b9ad127ec74
xblxgxd <octopus10032005@gmail.com>
2024-12-16 17:17:53 +0300

    feat: initial commit

---
+++ js/sideBar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").classList.add("open");
+  document.getElementById("backdrop").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").classList.remove("open");
+  document.getElementById("backdrop").style.display = "none";
+}
